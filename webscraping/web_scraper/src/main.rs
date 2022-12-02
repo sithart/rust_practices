041c2d24e6a81c67627bab71657bd3913e941563
sithart <siddhumirudhul@gmail.com>
2022-12-03 00:05:50 +0530

    webscraper in rust

---
+++ webscraping/web_scraper/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    
+    let response = reqwest::blocking::get("https://www.imdb.com/search/title/?groups=top_100&sort=user_rating,desc&count=100",)
+    .unwrap()
+    .text()
+    .unwrap();
+
+
+println!("{}", response)
+
+}
