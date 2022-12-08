cc42d44a566cd591e45f9030d1086995340f68e1
sithart <siddhumirudhul@gmail.com>
2022-12-08 22:00:56 +0530

    rust server init

--- api_server/src/main.rs
+++ api_server/src/main.rs
@@ -4,7 +4,7 @@ use warp::Filter;
 async fn main() {
     // GET /hello/warp => 200 OK with body "Hello, warp!"
     let hello = warp::path!("hello" / String)
-        .map(|name| format!("Hello, {}!", name));
+        .map(|name| format!("Let's say hi to , {}!", name));
 
     warp::serve(hello)
         .run(([127, 0, 0, 1], 3030))
