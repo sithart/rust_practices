bba7845f6977b1562fb00f253dbb5d9cb08267d1
sithart <siddhumirudhul@gmail.com>
2023-01-01 23:59:27 +0530

    update

--- primitives/src/main.rs
+++ primitives/src/main.rs
@@ -1,7 +1,10 @@
 fn main() {
     let logical: bool = true;
 
-    let a_flot: f64 = 1.0;
-    let an_integer =  5i32;
+    let a_flot: f64 = 1.0; // Regular annotation
+    let an_integer =  5i32; // Suffix annotation
+
+    let default_float = 3.0 // f64
     
+
 }
